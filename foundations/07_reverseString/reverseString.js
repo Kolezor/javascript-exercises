66e7aa6f37f71a0bb9f3849753e9316d5dd8d2b4
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-04-06 13:15:58 +0200

    Solve 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split("");
+    const reversedChars = [];
+    for (let i = chars.length - 1; i >= 0; i--) {
+        reversedChars.push(chars[i]);
+    }
+    return reversedChars.join("")
 };
 
 // Do not edit below this line
