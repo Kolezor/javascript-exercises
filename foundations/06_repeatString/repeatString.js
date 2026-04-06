d5d92f886f674d26b51c7c177010df6ca6dbc757
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-04-06 13:05:34 +0200

    Solve 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, multiplier) {
+    const arr = [];
+    if ( multiplier < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < multiplier; i++) {
+        arr.push(string);
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
