b34ceba65f22d7d0b18178ead520c8fc7e59e288
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-04-14 20:13:30 +0200

    WIP on 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
+    const arr = arguments[0];
+    const args = Array.from(arguments).slice(1);
+    const index = arr.indexOf(args[0]);
+    console.log(index);
 };
+
+// testArguments: [1, 2, 3, 4], 3
 
 // Do not edit below this line
 module.exports = removeFromArray;
