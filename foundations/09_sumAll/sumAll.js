6bc6cd56d51a5fa5774f338ec99d664e6b640a5e
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-05-02 22:45:04 +0200

    Solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if (!args.every((item) => Number.isInteger(item))
+        || !args.every((item) => item > 0) 
+    ) {
+        return "ERROR";
+    }
+    
+    let total = 0
+    for (let i = Math.min(...args); i <= Math.max(...args); i++) {
+        total += i;
+    }
+    return total; 
 };
 
 // Do not edit below this line
