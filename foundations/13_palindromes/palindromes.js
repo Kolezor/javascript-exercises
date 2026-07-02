82d32b99400000ace4d253be1493a4b6b2052be1
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-07-02 22:58:37 +0200

    Solve 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const cleaned = text.toLowerCase().replace(/[^a-z0-9]/g, '')
+    return cleaned === [...cleaned].reverse().join('')
 };
 
 // Do not edit below this line
