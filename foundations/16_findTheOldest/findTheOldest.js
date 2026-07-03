02d79be7672968d4d4254848e69b17b9008fa39b
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-07-04 01:21:37 +0200

    Solve 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentlyOldestPerson = {};
+    let currentlyOldestAge = 0;
+    people.forEach(person => {
+       let personAge = 0;
+       if ("yearOfDeath" in person) {
+        personAge = person.yearOfDeath - person.yearOfBirth;
+       } else {
+        personAge = (new Date()).getFullYear() - person.yearOfBirth;
+       }
+       
+       if (personAge > currentlyOldestAge) {
+        currentlyOldestAge = personAge;
+        currentlyOldestPerson = person;
+       }
+    })
 
+    return currentlyOldestPerson;
 };
 
 // Do not edit below this line
