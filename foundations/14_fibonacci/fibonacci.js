410bc3e95aa078ac9a90f094c6cda87961a13b82
Kolezor <133155154+Kolezor@users.noreply.github.com>
2026-07-03 18:03:08 +0200

    Solver 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    } else if (Number(number) === 0) {
+        return 0;
+    }
+    let a = 0, b = 1;
+    for (let i = 2; i <= Number(number); i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b
 };
 
 // Do not edit below this line
